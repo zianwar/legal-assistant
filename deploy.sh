6185cedadc981cc073ecf1be605df7e75f50c35f
Anwar Ziani <anwaar.ziani@gmail.com>
2023-06-15 23:57:59 -0700

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-git remote add legal-assistant.4nz.io dokku@dokku-4nz.westus.cloudapp.azure.com:legal-assistant.4nz.io
+git remote add legal-assistant dokku@dokku-4nz.westus.cloudapp.azure.com:legal-assistant
 
 export DOKKU_HOST=dokku-4nz.westus.cloudapp.azure.com
 
-dokku apps:create legal-assistant.4nz.io
+dokku apps:create legal-assistant
 dokku storage:ensure-directory legal-assistant --chown false
-dokku storage:mount legal-assistant.4nz.io /var/lib/dokku/data/storage/legal-assistant:/app/storage
+dokku storage:mount legal-assistant /var/lib/dokku/data/storage/legal-assistant:/app/storage
 
 # set env vars
-dokku config:export legal-assistant.4nz.io --envfile .env.prod
+dokku config:export legal-assistant --envfile .env.prod
 
-dokku config:set legal-assistant.4nz.io DATA_DIR=/app/storage OPENAI_API_KEY=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
+dokku config:set legal-assistant DATA_DIR=/app/storage OPENAI_API_KEY=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
 flyctl secrets set DATA_DIR=/data OPENAI_API_KEY=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
